49a601395b8618c6d642832b402cd315083276c6
Amit Sharma <coutamit.s19@gmail.com>
2023-06-14 11:10:16 +0000

    Removed formSpec

--- packages/enketo-express/public/js/src/module/connection.js
+++ packages/enketo-express/public/js/src/module/connection.js
@@ -152,14 +152,13 @@ async function _uploadBatch(recordBatch, record) {
         controller.abort();
     }, settings.timeout);
 
-    function getMeta(metaName) {
-        return document.querySelector(`meta[name=${metaName}]`).content;
-    }
+    // function getMeta(metaName) {
+    //     return document.querySelector(`meta[name=${metaName}]`).content;
+    // }
 
     // const formSpec = getMeta('formSpec') ;
-    const formSpec = `{ "skipOnSuccessMessage": true, "prefill": {}, "submissionURL": "http://esamwad.samagra.io/api/v4/form/submit", "name": "SampleForm", "successCheck": "async (formData) => { console.log(formData); }", "onSuccess": { "notificationMessage": "Form submitted successfully or not Maybe", "sideEffect": "async (formData) => { console.log(formData); }", "next": { "type": "form", "id": "form2" } }, "onFailure": { "message": "Form submission failed", "sideEffect": "async (formData) => { console.log(formData); }", "next": { "type": "url", "id": "google" } } }` ;
-    const formController = new FormController(JSON.parse(formSpec));
-    const response = await formController.processForm(record.xml, record.files);
+    const formController = new FormController();
+    const response = await formController.processFormNew(record.xml, record.files);
     // TODO: transforrm the form response
     // TODO: submit the form
     formController.broadcastFormData();
